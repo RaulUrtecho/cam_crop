870a1167ecf053dbd145f76cefc36e7287bec14a
Raul <raul.urtecho512@gmail.com>
2020-09-05 11:25:58 -0500

    Se configura app solo en portraid mode.

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,13 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cam_crop/camera_identification.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Obtain a list of the available cameras on the device.
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   final cameras = await availableCameras();
   // Get a specific camera from the list of available cameras.
   final firstCamera = cameras.first;
